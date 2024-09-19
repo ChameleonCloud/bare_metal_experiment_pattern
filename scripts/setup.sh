7f31b092e2268581d245f61d9c5a8a7f471464e8
= <markpowers@uchicago.edu>
2024-09-19 15:13:09 +0000

    Update experiment and scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Setting up experimental environment"
-echo
 
-sudo apt update -y
-sudo apt install -y stress-ng python3-swiftclient "linux-tools-$(uname -r)"
+set -x
+
+sudo apt-get update -yqq
+sudo apt-get install -qq -y stress-ng python3-swiftclient "linux-tools-$(uname -r)"
 sudo modprobe intel_rapl_msr
 
 
